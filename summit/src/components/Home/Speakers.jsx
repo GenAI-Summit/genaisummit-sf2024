da237f6f65e0bb88ec736e6f3c29b98203749635
lynchee-owo <lynchee.owo@gmail.com>
2023-06-21 21:25:11 -0700

    updated speakers

--- summit/src/components/Home/Speakers.jsx
+++ summit/src/components/Home/Speakers.jsx
@@ -13,41 +13,8 @@ const Speakers = () => {
                 </div>
 
                 <div className="row">
-                    <div className="col-lg-3 col-sm-6">
-                        <div className="single-speakers-box">
-                            <div className="speakers-image">
-                                <img 
-                                    src={require("../../assets/images/jerryliu.jpg")}
-                                    alt="speaker" 
-                                />
-                            </div>
 
-                            <div className="speakers-content">
-                                <h3>Jerry Liu</h3>
-                                <span>Co-founder & CEO @Llama Index </span>
-
-                                <ul className="social">
-                                    <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="https://jerryjliu.github.io/">
-                                            <i className="icofont-web"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/jerryjliu0">
-                                            <i className="icofont-twitter"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jerry-liu-64390071/">
-                                            <i className="icofont-linkedin"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-lg-3 col-sm-6">
+                <div className="col-lg-3 col-sm-6">
                         <div className="single-speakers-box">
                             <div className="speakers-image">
                                 <img 
@@ -58,7 +25,7 @@ const Speakers = () => {
 
                             <div className="speakers-content">
                                 <h3>CC Gong</h3>
-                                <span>Founder @Montage</span>
+                                <span>Co-Founder of Cerebral AI</span>
 
                                 <ul className="social">
                                     <li>
@@ -68,6 +35,40 @@ const Speakers = () => {
                                     </li>
                                     <li>
                                         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ccxgong/">
+                                            <i className="icofont-linkedin"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                <div className="col-lg-3 col-sm-6">
+                        <div className="single-speakers-box">
+                            <div className="speakers-image">
+                                <img 
+                                    src={require("../../assets/images/stevehoffman.jpeg")}
+                                    alt="speaker" 
+                                />
+                            </div>
+
+                            <div className="speakers-content">
+                                <h3>Steve Hoffman</h3>
+                                <span>Founder & CEO of Founders Space</span>
+
+                                <ul className="social">
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.foundersspace.com/hoffman/">
+                                            <i className="icofont-web"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/FoundersSpace">
+                                            <i className="icofont-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/foundersspace/">
                                             <i className="icofont-linkedin"></i>
                                         </a>
                                     </li>
@@ -87,7 +88,7 @@ const Speakers = () => {
 
                             <div className="speakers-content">
                                 <h3>Yangqing Jia</h3>
-                                <span> VP @AlibabaGroup </span>
+                                <span> VP of AlibabaGroup, ex Meta AI Lead </span>
 
                                 <ul className="social">
                                     <li>
@@ -114,6 +115,40 @@ const Speakers = () => {
                         <div className="single-speakers-box">
                             <div className="speakers-image">
                                 <img 
+                                    src={require("../../assets/images/jerryliu.jpeg")}
+                                    alt="speaker" 
+                                />
+                            </div>
+
+                            <div className="speakers-content">
+                                <h3>Jerry Liu</h3>
+                                <span>Co-founder & CEO of Llama Index </span>
+
+                                <ul className="social">
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://jerryjliu.github.io/">
+                                            <i className="icofont-web"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/jerryjliu0">
+                                            <i className="icofont-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jerry-liu-64390071/">
+                                            <i className="icofont-linkedin"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-3 col-sm-6">
+                        <div className="single-speakers-box">
+                            <div className="speakers-image">
+                                <img 
                                     src={require("../../assets/images/owyang.jpeg")} 
                                     alt="speaker" 
                                 />
@@ -121,7 +156,7 @@ const Speakers = () => {
 
                             <div className="speakers-content">
                                 <h3>Jeremiah Owyang</h3>
-                                <span> Founder @Catalyst Companies </span>
+                                <span> Twitter Tech KOL </span>
 
                                 <ul className="social">
                                     <li>
@@ -144,39 +179,75 @@ const Speakers = () => {
                         </div>
                     </div>
 
-                    {/* <div className="col-lg-3 col-sm-6">
+
+
+                    <div className="col-lg-3 col-sm-6">
                         <div className="single-speakers-box">
                             <div className="speakers-image">
                                 <img 
-                                    src={require("../../assets/images/speakers5.jpg")}
+                                    src={require("../../assets/images/lianminzheng.jpeg")}
                                     alt="speaker" 
                                 />
                             </div>
 
                             <div className="speakers-content">
-                                <h3>Lima</h3>
-                                <span>xxx</span>
+                                <h3>Lianmin Zheng</h3>
+                                <span>Author of Vicuna, UCB LMSYS AI Lab</span>
 
                                 <ul className="social">
                                     <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="">
+                                        <a target="_blank" rel="noopener noreferrer" href="https://lmzheng.net/">
                                             <i className="icofont-web"></i>
                                         </a>
                                     </li>
                                     <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="">
+                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/lm_zheng">
                                             <i className="icofont-twitter"></i>
                                         </a>
                                     </li>
                                     <li>
-                                        <a target="_blank" rel="noopener noreferrer" href="">
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lianmin-zheng-6266a8114">
                                             <i className="icofont-linkedin"></i>
                                         </a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                    </div> */}
+                    </div>
+
+                    <div className="col-lg-3 col-sm-6">
+                        <div className="single-speakers-box">
+                            <div className="speakers-image">
+                                <img 
+                                    src={require("../../assets/images/chuntingzhou.jpeg")}
+                                    alt="speaker" 
+                                />
+                            </div>
+
+                            <div className="speakers-content">
+                                <h3>Chunting Zhou</h3>
+                                <span>Creator of LIMA, CMU LTI</span>
+
+                                <ul className="social">
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://violet-zct.github.io">
+                                            <i className="icofont-web"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/violet_zct">
+                                            <i className="icofont-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/chunting-zhou-14525985">
+                                            <i className="icofont-linkedin"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
 
                     <div className="col-lg-3 col-sm-6">
                         <div className="single-speakers-box">

5d7222d3a3cecc05120ddeeec0d8533f18c5d46c
Daniel777y <yudexu123@126.com>
2024-05-09 18:37:59 +0000

    add sponsor

--- summit/src/page/Index.vue
+++ summit/src/page/Index.vue
@@ -1510,6 +1510,11 @@ export default {
         url: "https://gen-x.co.jp/",
       },
       {
+        img: new URL(`../assets/images/sponsors/pinecone.png`, import.meta.url)
+          .href,
+        url: "https://www.pinecone.io/",
+      },
+      {
         img: new URL(
           `../assets/images/sponsors/chatmaster.png`,
           import.meta.url

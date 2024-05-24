2820411305757db8e7cab8d3daa2c9bee3df6600
Daniel777y <yudexu123@126.com>
2024-05-24 21:16:14 +0000

    update workshop ui

--- summit/src/components/Workshop.vue
+++ summit/src/components/Workshop.vue
@@ -4,19 +4,28 @@
     :class="screenWidth < 600 ? 'mb' : ''"
     data-aos="fade-up"
   >
-    <div
-      class="workshop-item"
-      v-for="item in workshop"
-      :key="item.name"
-    >
+  <div
+    class="workshop-item"
+    v-for="item in workshop"
+    :key="item.name"
+  >
+    <div class="workshop-pic">
       <img
-        class="workshop-pic"
         :src="getImage(item.image)"
-        alt="workshop image" 
-        @click="openUrl(item.url)" 
+        alt="workshop image"
+        @click="openUrl(item.url)"
         loading="lazy"
       />
     </div>
+    <div class="workshop-info">
+      <div class="workshop-title">
+        Topic: {{ item.topic ? item.topic : `Will be provided by speakers soon`}}
+      </div>
+      <div class="workshop-description">
+        Description: {{ item.description ? item.description : `Will be provided by speakers soon` }}
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -64,38 +73,80 @@ export default {
 
   &.mb {
     .workshop-item {
-      margin-bottom: 0.3rem;
-      height: 100%;
-      margin-left: 0.1rem;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      margin-top: 0.2rem;
+      margin-bottom: 0.2rem;
       .workshop-pic {
-        width: 90%;
-        height: auto;
+        width: 35%;
+      }
+      .workshop-info {
+        width: 100%;
+        border-radius: 0rem;
       }
     }
   }
 
   .workshop-item {
     display: flex;
-    justify-content: center;
-    width: 48%;
-    background-color: #ffffff;
+    align-items: flex-start;
+    width: 100%;
     text-align: center;
-    cursor: pointer;
-    height: 1.8rem;
+    height: 100%;
     z-index: 2;
-    border-radius: 1.5rem;
-    margin-left: 1%;
     margin-top: 0.2rem;
     margin-bottom: 0.2rem;
-    transition: all linear 0.3s;
+    background-color: #ffffff;
+    border-radius: 0.2rem;
 
-    &:hover {
-      background: #008aff;
-    }
+
     .workshop-pic {
-      height: 1.8rem;
+      background-color: #ffffff;
+      border-radius: 1.5rem;
+      width: 20%;
       z-index: -1;
       display: block;
+      transition: all linear 0.3s;
+      cursor: pointer;
+      margin-left: 0.1rem;
+      margin-top: 0.1rem;
+
+      img {
+        width: 80%;
+        height: auto;
+        margin-left: 0.2rem;
+      }
+
+      &:hover {
+        background: #008aff;
+      }
+    }
+    
+    .workshop-info {
+      width: 75%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      text-align: left;
+      font-size: 2rem;
+      color: #000000;
+      margin-left: 0.2rem;
+      margin-top: 0.1rem;
+      background-color: #ffffff;
+      padding: 0.2rem;
+      border-radius: 0.2rem;
+
+      .workshop-title {
+        font-family: HarmonyOS Sans SC Bold;
+      }
+
+      .workshop-description {
+        margin-top: 0.2rem;
+        font-family: HarmonyOS Sans SC Regular;
+      }
     }
   }
 }
